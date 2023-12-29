049eaa6d418ec3098e3a16daf13ab78c962687b3
TuanTNT19 <tuantnt08@gmail.com>
2023-12-30 05:45:24 +0700

    add run double command feature

--- main.c
+++ main.c
@@ -1,5 +1,4 @@
 
-
 // C Program to design a shell in Linux 
 #include<stdio.h> 
 #include<string.h> 
@@ -15,6 +14,8 @@
 
 char input[50];
 char *test[] = {"ls","-l",NULL};
+char *command1[10] = {"ps", "-aux"};
+char *command2[10] = {"grep", "test"};
 
 void init()
 {
@@ -51,7 +52,7 @@ void printfCurDir()
     printf("%s\n",dir);
 }
 
-void runcommad(char *str[])
+void runsinglecommad(char *str[])
 {
     pid_t child_pid;
     child_pid = fork();
@@ -76,14 +77,71 @@ void runcommad(char *str[])
     }
 }
 
+void rundoublecommand(char **command1, char **command2)
+{
+    pid_t pid1, pid2;
+    int fds[2];
+
+    if (pipe(fds) < 0)
+    {
+        printf("ERROR: Can not create pipe\n");
+        return ;
+    }
+
+    pid1 = fork();
+    
+    if (pid1 < 0)
+    {
+        printf("ERROR: Can not fork\n");
+        return ;
+    }
+    else if (pid1 == 0)
+    {
+        close(fds[0]);
+        dup2(fds[1], STDOUT_FILENO);
+        close(fds[1]);
+
+        if (execvp(command1[0], command1) < 0){
+            printf("ERROR: Can not run command 1\n");
+            return ;
+        }
+        exit(0);
+    }
+    else{
+        pid2 = fork();
+        
+        if (pid2 < 0)
+        {
+            printf("ERROR: Can not fork\n");
+            return ;
+        }
+        else if (pid2 == 0)
+        {
+            close(fds[1]);
+            dup2(fds[0], STDIN_FILENO);
+            close(fds[0]);
+
+            if (execvp(command2[0], command2) < 0)
+            {
+                printf("ERROR: Can not run command 2\n");
+                return ;
+            }
+        }
+        else{
+            wait(NULL);
+            wait(NULL);
+        }
+    }
+}
+    
 
 int main()
 {
     init();
     takeInput(input);
     printfCurDir();
-    runcommad(test);
+    runsinglecommad(test);
+    rundoublecommand(command1, command2);
    
- 
     return 0;
 }
